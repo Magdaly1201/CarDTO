0025d9ae537dc5c79ccf1923ec77c3b39676db13
Magdy1201 <magdaly1201@gmail.com>
2020-08-24 16:44:09 -0300

    first commit
    first commit

---
+++ src/main/java/com/example/demo/car/dto/ComExampleCarDtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.car.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComExampleCarDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComExampleCarDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/car/dto/ComExampleCarDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.car.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComExampleCarDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
